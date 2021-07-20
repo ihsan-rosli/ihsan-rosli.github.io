66ac14612adcef8064217fa35cb6e074e366c205
l-GRIN <67753912+l-GRIN@users.noreply.github.com>
2021-07-21 00:54:58 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+/* simpan class html dalam var */
+
+navToggle.addEventListener('click', () => { 
+    /* apa tindakan akan kita buat kat var tu */
+
+    document.body.classList.toggle('nav-open');
+    /* bila tindakan dibuat, css ni akan aktif */
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+        /* bila tindakan dibuat, css ni akan disable */
+    })
+})
